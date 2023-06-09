69813ae2c3fb26478de0d3fdb3fe80ddc9859ce8
Sean Galland <s_galland@msn.com>
2023-06-09 12:37:17 -0600

    Fixed event processing

---
+++ src/engine.zig
@@ -0,0 +1,75 @@
+const std = @import("std");
+const c = @cImport({
+    @cInclude("SDL.h");
+});
+
+pub const Graphics = struct {
+    const Self = @This();
+
+    window: ?*c.SDL_Window,
+    surface: [*c]c.SDL_Surface,
+    texture: ?*c.SDL_Texture,
+    renderer: ?*c.SDL_Renderer,
+    video_pitch: c_int,
+
+    pub fn init(title: [*c]const u8, width: c_int, height: c_int, buffer_width: c_int, buffer_height: c_int) Self {
+        if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
+            std.debug.print("SDL failed to initialize: {s}\n", .{c.SDL_GetError()});
+            std.os.exit(0);
+        }
+
+        var graphics = Graphics{
+            .window = undefined,
+            .surface = undefined,
+            .texture = undefined,
+            .renderer = undefined,
+            .video_pitch = @sizeOf(c_int) * buffer_width,
+        };
+
+        graphics.window = c.SDL_CreateWindow(title, c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, width, height, c.SDL_WINDOW_SHOWN);
+        graphics.renderer = c.SDL_CreateRenderer(graphics.window, -1, c.SDL_RENDERER_ACCELERATED);
+        graphics.surface = c.SDL_CreateRGBSurface(0, buffer_width, buffer_height, 32, 0, 0, 0, 0);
+        graphics.texture = c.SDL_CreateTexture(graphics.renderer, graphics.surface.*.format.*.format, c.SDL_TEXTUREACCESS_STREAMING, graphics.surface.*.w, graphics.surface.*.h);
+
+        return graphics;
+    }
+
+    pub fn update(self: *Self, buffer: ?*const anyopaque) void {
+        _ = c.SDL_UpdateTexture(self.texture, null, buffer, self.video_pitch);
+        _ = c.SDL_RenderClear(self.renderer);
+        _ = c.SDL_RenderCopy(self.renderer, self.texture, null, null);
+        c.SDL_RenderPresent(self.renderer);
+    }
+
+    pub fn free(self: *Self) void {
+        c.SDL_Quit();
+        c.SDL_DestroyWindow(self.window);
+        c.SDL_FreeSurface(self.surface);
+        c.SDL_DestroyTexture(self.texture);
+    }
+};
+
+pub const EventType = std.meta.Tuple(&.{ u32, bool });
+
+pub const Event = struct {
+    const Self = @This();
+
+    events: []EventType,
+
+    pub fn init(events: []EventType) Self {
+        if (c.SDL_Init(c.SDL_INIT_EVENTS) != 0) {
+            std.debug.print("SDL failed to initialize: {s}\n", .{c.SDL_GetError()});
+            std.os.exit(0);
+        }
+
+        return Event{ .events = events };
+    }
+
+    pub fn getKeyPressed(self: *Self, key_pressed: u32) bool {
+        _ = self;
+
+        c.SDL_PumpEvents();
+        const key_state = c.SDL_GetKeyboardState(null);
+        return key_state[key_pressed] > 0;
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const c = @cImport({
     @cInclude("SDL.h");
 });
 const chip8 = @import("chip8.zig");
+const engine = @import("engine.zig");
 
 pub fn main() !void {
     // Make user configurable
@@ -26,58 +27,15 @@ pub fn main() !void {
     var random_generator = std.rand.DefaultPrng.init(now.timestamp);
     const random = random_generator.random();
 
+    var graphics = engine.Graphics.init("zCHIP8", chip8.DISPLAY_WIDTH * VIDEO_SCALE, chip8.DISPLAY_HEIGHT * VIDEO_SCALE, chip8.DISPLAY_WIDTH, chip8.DISPLAY_HEIGHT);
+    defer graphics.free();
+
+    var events = [_]engine.EventType{.{ .@"0" = c.SDL_QUIT, .@"1" = false }};
+    var event_e = engine.Event.init(&events);
+
     // Start emulation
     var instance = chip8.Chip8.init(allocator, random);
     try instance.loadRom("IBM Logo.ch8");
-
-    // Initialize SDL
-    _ = c.SDL_Init(c.SDL_INIT_VIDEO);
-    defer c.SDL_Quit();
-
-    var window = c.SDL_CreateWindow("zCHIP8", c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, chip8.DISPLAY_WIDTH * VIDEO_SCALE, chip8.DISPLAY_HEIGHT * VIDEO_SCALE, c.SDL_WINDOW_SHOWN);
-    defer c.SDL_DestroyWindow(window);
-
-    var renderer = c.SDL_CreateRenderer(window, -1, c.SDL_RENDERER_ACCELERATED);
-    defer c.SDL_DestroyRenderer(renderer);
-
-    const surface = c.SDL_CreateRGBSurface(0, chip8.DISPLAY_WIDTH, chip8.DISPLAY_HEIGHT, 32, 0, 0, 0, 0);
-    defer c.SDL_FreeSurface(surface);
-
-    _ = c.SDL_SetSurfaceRLE(surface, c.SDL_TRUE);
-
-    const texture = c.SDL_CreateTexture(renderer, surface.*.format.*.format, c.SDL_TEXTUREACCESS_STREAMING, surface.*.w, surface.*.h);
-    defer c.SDL_DestroyTexture(texture);
-
-    //  The pitch size must be fron c_int, otherwise its just wrong
-    const video_pitch = @sizeOf(u32) * chip8.DISPLAY_WIDTH;
-
-    var step: usize = 0;
-    _ = step;
-
-    //
-    //
-    // TEST CODE
-    //
-    //
-    const foo = try std.time.Instant.now();
-    var prgn = std.rand.DefaultPrng.init(foo.timestamp);
-    const rnd = prgn.random();
-    _ = rnd;
-
-    var text_buffer: [chip8.DISPLAY_WIDTH * chip8.DISPLAY_HEIGHT]u32 = [_]u32{0} ** (chip8.DISPLAY_WIDTH * chip8.DISPLAY_HEIGHT);
-    for (0..chip8.DISPLAY_WIDTH) |x| {
-        for (0..chip8.DISPLAY_HEIGHT) |y| {
-            const pos = y * chip8.DISPLAY_WIDTH + x;
-
-            // if (x == step and y == step) {
-            //     text_buffer[pos] = rnd.intRangeAtMost(u32, 0xFF000000, 0xFFFFFFFF);
-            //     //0xFFFFFFFF;
-            //     step += 1;
-            // } else text_buffer[pos] = 0x00000000;
-
-            if (x == 0 or x == chip8.DISPLAY_WIDTH - 1 or y == 0 or y == chip8.DISPLAY_HEIGHT - 1) text_buffer[pos] = 0xFFFF0000;
-        }
-    }
 
     mainloop: while (true) {
         // Process events
@@ -89,16 +47,12 @@ pub fn main() !void {
             }
         }
 
+        if (event_e.getKeyPressed(c.SDL_SCANCODE_ESCAPE)) break :mainloop;
+
         const time_now = try std.time.Instant.now();
         instance.cycle(time_now.timestamp);
 
-        // Need to draw the pixels
-        // See: https://github.com/sgalland/SAGE-CPP/blob/master/src/backend/sdl2/Graphics.cpp
-
-        _ = c.SDL_UpdateTexture(texture, null, &instance.video, video_pitch);
-        _ = c.SDL_RenderClear(renderer);
-        _ = c.SDL_RenderCopy(renderer, texture, null, null);
-        c.SDL_RenderPresent(renderer);
+        graphics.update(&instance.video);
     }
 }
 
